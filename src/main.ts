f9c40d72a1d49b133b684dbcca2b4c835087140d
paokue <paokue77@gmail.com>
2023-08-06 14:13:53 +0700

    Update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api'); // this is when we want to add prefix to every routes
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
   await app.listen(3000);
 }
 bootstrap();
